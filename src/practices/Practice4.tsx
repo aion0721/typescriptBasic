8a3ccf8586f4d6346f5544a848d90b9a800bb242
a <rudolf.aion@gmail.com>
2022-10-08 16:38:48 +0000

    basic fix

---
+++ src/practices/Practice4.tsx
@@ -0,0 +1,16 @@
+export const Practice4 = () =>{
+    const calcTotalFee = (num:any) => {
+        const total = num *1.1;
+        console.log(total);
+    }
+    const onClickPractice = () => {
+        calcTotalFee(1000)
+    }
+    return(
+        <div>
+            <p>Practice1</p>
+            <button onClick={onClickPractice}>Exec</button>
+        </div>
+
+    )
+}
